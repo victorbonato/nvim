7e421b3a59a95303c242b88c5de20dfaaf7debd8
Victor <victorbonatoit@gmail.com>
2023-12-30 14:59:49 -0300

    enable ts-autotag

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    opts = {
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+      },
+    },
+  },
+}
