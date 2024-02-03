e04629cd54f849fdb0c13f8bfcc3d4470736a603
Victor <victorbonatoit@gmail.com>
2024-02-03 14:34:14 -0300

    Disable copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
