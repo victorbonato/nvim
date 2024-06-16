c540ab80620c82e9c8fbf9b16401e45e54d91353
Victor <victorbonatoit@gmail.com>
2024-06-16 19:51:35 -0300

    Set tab to 4 to fix wrong indentation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,8 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 vim.opt.scrolloff = 10
 
 vim.opt.clipboard = ""
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
